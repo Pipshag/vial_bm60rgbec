5eca5c231c1beb5d942fe98d964cb67805321f60
Pipshag <konton@mjau.cc>
2025-08-05 19:06:56 +0200

    Vial visual layout

--- config.h
+++ config.h
@@ -2,32 +2,18 @@
 
 #pragma once
 
-/* Maybe this works for correct pins? */
-/* key matrix size */
-// #define MATRIX_ROWS 5
-// #define MATRIX_COLS 14
-// #define ENCODERS_PAD_A { F6 }
-// #define ENCODERS_PAD_B { F5 }
-// #define ENCODER_RESOLUTION 2
-
-// #define MATRIX_ROW_PINS { B0, B1, B2, B3, E6 }
-// #define MATRIX_COL_PINS { D0, D1, D2, D3, D5, D4, D6, D7, B4, B5, B6, C6, C7, F7 }
-
-// #define DIODE_DIRECTION COL2ROW
-
 #define RGB_DI_PIN E2
 #define DRIVER_LED_TOTAL 69
 #define RGBLED_NUM 69
 #define RGB_MATRIX_KEYPRESSES
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 140
 #define RGB_MATRIX_KEYPRESSES
-/* End */
 
-// #define ENCODER_RESOLUTION 2
 #define ENCODER_DIRECTION_FLIP
 
 #define VIAL_KEYBOARD_UID {0x79, 0xD7, 0xD3, 0x5D, 0xAF, 0x2B, 0xF5, 0x57}
 
+// Unlock VIAL with ESC and 4 (fifth key from left on the first row)
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 4, 0 }
 
